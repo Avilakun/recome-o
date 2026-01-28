99dae0b26e0ea520b6bce2f8be418c5448f1e38a
Avilakun <106196264+Avilakun@users.noreply.github.com>
2026-01-27 22:31:11 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './components/Header';
 import { InfoRow } from './components/InfoRow';
 import { VitalsRow } from './components/VitalsRow';
